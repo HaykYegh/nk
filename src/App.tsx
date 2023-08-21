5f0fb19e3585f7f37a57fbd9dac056805b5c8156
VaheGasparyan <vahegsparyan@gmail.com>
2023-08-21 14:04:02 +0400

    fix flex wrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import {OurOffice} from "./components/OurOffice";
+import { OurOffice } from './components/OurOffice';
 
 import styles from './App.module.scss';
 
 const App = () => {
   return (
-      <div>
-          <div className={styles.container}>
-              <OurOffice />
-          </div>
+    <div>
+      <div className={styles.container}>
+        <OurOffice />
       </div>
-  )
+    </div>
+  );
 };
 
 export default App;
