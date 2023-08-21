1fb3909584947f3923665f0f31cd12b83f926e44
suren0609 <suro.balayan@gmail.com>
2023-08-21 18:26:52 +0400

    img changed to background

--- src/components/HeaderImg/HeaderImg.tsx
+++ src/components/HeaderImg/HeaderImg.tsx
@@ -5,10 +5,6 @@ import styles from './HeaderImg.module.scss';
 const HeaderImg = () => {
   return (
     <div className={styles.HeaderImg}>
-      <img
-        src="https://images.pexels.com/photos/245032/pexels-photo-245032.jpeg"
-        alt=""
-      />
       <div className={styles.textOnImg}>
         <h1>SOFTWARE DEVELOPMENT AND INNOVATION</h1>
         <button className={styles.contactUsBtn}>CONTACT US</button>
