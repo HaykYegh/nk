d5727d84dfca40e1f6bb4ca31410fe044f6711ad
suren0609 <suro.balayan@gmail.com>
2023-08-24 10:50:09 +0400

    approach numbers fixed

--- src/pages/Approach/Approach.tsx
+++ src/pages/Approach/Approach.tsx
@@ -8,12 +8,16 @@ const Approach = () => {
       <h3>YOUR PROJECT IS JUST 4 EASY AND SIMPLE STEPS AWAY.</h3>
       <div className={styles.steps}>
         <div className={styles.step}>
-          <div className={styles.num}>1</div>
+          <div className={styles.num}>
+            <p>1</p>
+          </div>
           <h4>YOU TELL</h4>
           <p>You contact us and tell us about your project or idea.</p>
         </div>
         <div className={styles.step}>
-          <div className={styles.num}>2</div>
+          <div className={styles.num}>
+            <p>2</p>
+          </div>
           <h4>WE MEET</h4>
           <p>
             After getting your request we set a meeting and hold a video call to
@@ -21,7 +25,9 @@ const Approach = () => {
           </p>
         </div>
         <div className={styles.step}>
-          <div className={styles.num}>3</div>
+          <div className={styles.num}>
+            <p>3</p>
+          </div>
           <h4>YOU GET THE PROPOSAL</h4>
           <p>
             Analyzing the project scope we prepare the project proposal and send
@@ -29,7 +35,9 @@ const Approach = () => {
           </p>
         </div>
         <div className={styles.step}>
-          <div className={styles.num}>4</div>
+          <div className={styles.num}>
+            <p>4</p>
+          </div>
           <h4>WE DOCUMENT IT</h4>
           <p>
             After your confirmation we sign the contract and the project starts.
